430995fcce4fdadd07bb8af348ae431694c7787c
OptimalBlock489 <89468732+OptimalBlock489@users.noreply.github.com>
2022-05-30 15:18:02 +0530

    Update index.js
    cheesed

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 31, 2022',
+  birthdate: 'May 31, 2022',
   name: 'Dad'
 };
 
